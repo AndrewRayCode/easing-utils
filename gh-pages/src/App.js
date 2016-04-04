70e068acc0891f13464c2395b3e261c11db8462d
Andrew Ray <delvarworld@gmail.com>
2016-04-03 17:56:16 -0700

    diner

--- gh-pages/src/App.js
+++ gh-pages/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from 'react';
+import * as easing from '../src/easing';
 
 export default class App extends Component {
 
     render() {
+        console.log(easing, Object.keys(easing));
         return <div>
             Hello world!
         </div>;
