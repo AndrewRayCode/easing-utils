dba4e1b4b55185080fbbea777642f9fe26a3719f
Andrew Ray <delvarworld@gmail.com>
2016-04-03 17:21:58 -0700

    more shit

---
+++ gh-pages/index.js
@@ -0,0 +1,3 @@
+import App from './src/App';
+
+ReactDOM.render( <App />, document.getElementById( 'app' ) );
--- gh-pages/src/App.js
+++ gh-pages/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class App extends Component {
+
     render() {
         return <div>
             Hello world!
         </div>;
     }
+
 }
