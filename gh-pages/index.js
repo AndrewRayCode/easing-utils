69151247ae664b311157bf9e80723cfbfc9cb69e
Andrew Ray <delvarworld@gmail.com>
2016-04-03 17:54:08 -0700

    dlkjer

--- gh-pages/index.js
+++ gh-pages/index.js
@@ -1,4 +1,4 @@
 import App from './src/App';
+import ReactDOM from 'react-dom';
 
-console.log('rendering in to ',document.getElementById( 'app' ));
 ReactDOM.render( <App />, document.getElementById( 'app' ) );
