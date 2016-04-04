59824e034c5053603b27640391eb3c461ed8dbe4
Andrew Ray <delvarworld@gmail.com>
2016-04-03 17:19:57 -0700

    initial commit for gh pages shit

---
+++ gh-pages/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class App extends Component {
+    render() {
+        return <div>
+            Hello world!
+        </div>;
+    }
+}
